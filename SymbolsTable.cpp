67413ee1ee9dbf04ea8ee4652f9e3f4210fa138a
DanteCampos <dcamposfsa>
2022-06-18 00:57:16 -0300

    Palavras reservadas adicionadas

--- SymbolsTable.cpp
+++ SymbolsTable.cpp
@@ -1,12 +1,31 @@
 #pragma once
 #include <unordered_map>
 #include <string>
+#include <vector>
 #include "Token.cpp"
 
 class SymbolsTable{
     std::unordered_map<std::string, std::string, std::string> table;
 
     public:
+        SymbolsTable(){
+            std::vector<Token> reservedWords;
+            reservedWords.push_back(Token("inicio", "inicio", "", 0, 0));
+            reservedWords.push_back(Token("varinicio", "varinicio", "", 0, 0));
+            reservedWords.push_back(Token("varfim", "varfim", "", 0, 0));
+            reservedWords.push_back(Token("escreva", "escreva", "", 0, 0));
+            reservedWords.push_back(Token("leia", "leia", "", 0, 0));
+            reservedWords.push_back(Token("se", "se", "", 0, 0));
+            reservedWords.push_back(Token("entao", "entao", "", 0, 0));
+            reservedWords.push_back(Token("fimse", "fimse", "", 0, 0));
+            reservedWords.push_back(Token("Repita", "Repita", "", 0, 0));
+            reservedWords.push_back(Token("fimRepita", "fimRepita", "", 0, 0));
+            reservedWords.push_back(Token("fim", "fim", "", 0, 0));
+            reservedWords.push_back(Token("inteiro", "inteiro", "", 0, 0));
+            reservedWords.push_back(Token("literal", "literal", "", 0, 0));
+            reservedWords.push_back(Token("real", "real", "", 0, 0));
+        }
+
         bool putSymbol(Token tk){
 
         }
